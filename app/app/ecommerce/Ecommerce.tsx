9077804e8b7dec051c58ab2ff01421c3c074c520
felipebs07 <07felipebs07@gmail.com>
2025-09-06 07:23:39 -0300

    Adicionando projeto base frontend com React

---
+++ app/app/ecommerce/Ecommerce.tsx
@@ -0,0 +1,11 @@
+export function EcommerceApp() {
+    return (
+        <div>
+            <header>
+                <div>
+                    <h1>Sankhya E-Commerce</h1>
+                </div>
+            </header>
+        </div>
+    )
+}
